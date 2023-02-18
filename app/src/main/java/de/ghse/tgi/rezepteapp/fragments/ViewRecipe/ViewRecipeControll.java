b718a2eaf08d63e05414fee6e4fe68a8cebdbf36
NaschTheNachan04 <119351672+NaschTheNachan04@users.noreply.github.com>
2023-02-18 18:17:01 +0100

    Sehr geehrter Herr Süer Sie haben einen Fehler Gemacht lg. Finn

--- app/src/main/java/de/ghse/tgi/rezepteapp/fragments/ViewRecipe/ViewRecipeControll.java
+++ app/src/main/java/de/ghse/tgi/rezepteapp/fragments/ViewRecipe/ViewRecipeControll.java
@@ -1,6 +1,10 @@
 package de.ghse.tgi.rezepteapp.fragments.ViewRecipe;
 
+import java.util.ArrayList;
+
 import de.ghse.tgi.rezepteapp.MainActivity;
+import de.ghse.tgi.rezepteapp.R;
+import de.ghse.tgi.rezepteapp.Recipe;
 import de.ghse.tgi.rezepteapp.StorageRecipe;
 import de.ghse.tgi.rezepteapp.fragments.ListRecipe.ListRecipeFragment;
 
@@ -15,8 +19,15 @@ public class ViewRecipeControll {
         this.storage = main.getStorage();
     }
     public void onCreate(){
-        gui.setDescription(storage.getList().get(ListRecipeFragment.getClickedItem()).getDesription());
-        gui.setRName(storage.getList().get(ListRecipeFragment.getClickedItem()).getName());
-        gui.setImage(storage.getList().get(ListRecipeFragment.getClickedItem()).getImage());
+        ArrayList<Recipe> list = storage.getList();
+        if(list.size() == 0){
+            gui.setRName("No Recipe");
+            gui.setDescription("No Description");
+            gui.setImage(R.drawable.mealpic);
+        }else {
+            gui.setDescription(list.get(ListRecipeFragment.getClickedItem()).getDesription());
+            gui.setRName(storage.getList().get(ListRecipeFragment.getClickedItem()).getName());
+            gui.setImage(storage.getList().get(ListRecipeFragment.getClickedItem()).getImage());
+        }
     }
 }
