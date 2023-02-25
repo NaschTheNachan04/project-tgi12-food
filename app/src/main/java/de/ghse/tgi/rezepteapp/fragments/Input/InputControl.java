ae80d9b35384e852d8fda6a98ae2c279c38219c0
clovehitch5 <119538886+clovehitch5@users.noreply.github.com>
2023-02-25 16:27:46 +0100

    Rezepte suchen und in der Liste anzeigen

---
+++ app/src/main/java/de/ghse/tgi/rezepteapp/fragments/Input/InputControl.java
@@ -0,0 +1,25 @@
+package de.ghse.tgi.rezepteapp.fragments.Input;
+
+import de.ghse.tgi.rezepteapp.Recipe;
+import de.ghse.tgi.rezepteapp.StorageRecipe;
+
+public class InputControl {
+    private StorageRecipe storage;
+    private InputFragment gui;
+
+    public InputControl(InputFragment pGui, StorageRecipe s){
+        gui = pGui;
+        storage = s;
+    }
+
+    public void save(){
+        if(!gui.getRecipeName().isEmpty()) {
+            Recipe a = new Recipe();
+            a.setName(gui.getRecipeName());
+            a.setDescription(gui.getRecipeDescription());
+            a.setIngredient(gui.getRecipeIngredients());
+            storage.addRecipe(a);
+        }
+        gui.clearTextFields();
+    }
+}
