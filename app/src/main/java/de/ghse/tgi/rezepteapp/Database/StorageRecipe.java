75aab91f19d4a50c3d77a19b1712b2fcf5fa2e45
NaschTheNachan04 <119351672+NaschTheNachan04@users.noreply.github.com>
2023-06-08 21:24:19 +0200

    Database noch nicht fertig verbessert(noch nicht getestet) Änderung: -Neue Klassen zur Lesen der Datenbank(DataBaseReaderRecpe/Ingredient,ähnlich wie Recipe und Ingredient) -Storage befehle angepasst

--- app/src/main/java/de/ghse/tgi/rezepteapp/Database/StorageRecipe.java
+++ app/src/main/java/de/ghse/tgi/rezepteapp/Database/StorageRecipe.java
@@ -12,12 +12,9 @@ import de.ghse.tgi.rezepteapp.Recipe;
 public class StorageRecipe {
     private ArrayList<Recipe> list = new ArrayList<>();
     private ArrayList<Integer> emptyList = new ArrayList<>();
-
-
-
-
-
     public AppDatabase database;
+    public ArrayList<DatabaseReaderRecipe> recipeReader = new ArrayList<>();
+    public ArrayList<DatabaseReaderIngredient> ingredientReader = new ArrayList<>();
 
 
     public void addContext(Context c){
@@ -37,56 +34,79 @@ public class StorageRecipe {
         emptyList.remove(emptyList.size()-1);
     }
     public int getCount(){
-        return list.size();
+        //return list.size();
+        recipeReader=database.getRecipes();
+        return  recipeReader.size();
+
     }
 
     public ArrayList<Integer> getFilteredIndexList(String filter){
+        recipeReader=database.getRecipes();
         filter.toLowerCase();
         ArrayList<Integer> filteredList = new ArrayList<Integer>();
-        for (int i=0;i<list.size();i++){
-            if (list.get(i).getName().toLowerCase().contains(filter)){
+        for (int i=0;i<recipeReader.size();i++){
+            if (recipeReader.get(i).getNameRecipe().toLowerCase().contains(filter)){
                 filteredList.add(i);
             }
         }
         return filteredList;
     }
-    public String getRecipeName(int index){return list.get(index).getName();}
-    public int getRecipeImage(int index){return list.get(index).getImage();}
-    public String getRecipeDescription(int index){return list.get(index).getDescription();}
-    public ArrayList<String> getIngredients(){
-        ArrayList<String> list = new ArrayList<String>();
+    public String getRecipeName(int index){
+        //return list.get(index).getName();
+        recipeReader=database.getRecipes();
+        return recipeReader.get(index).getNameRecipe();
+    }
+    public int getRecipeImage(int index){
+        //return list.get(index).getImage();
+        recipeReader=database.getRecipes();
+        return recipeReader.get(index).getImageRecipe();
+    }
+    public String getRecipeDescription(int index){
+        //return list.get(index).getDescription();
+        recipeReader=database.getRecipes();
+        return recipeReader.get(index).getDescritipionRecipe();
+    }
+    public ArrayList<DatabaseReaderIngredient> getIngredients(){
+        /*ArrayList<String> list = new ArrayList<String>();
         list.add("tomato");
         list.add("apple");
         list.add("egg");
         list.add("fish");
         list.add("cucumber");
         list.add("salmon");
-        return list;
+        return list;*/
+        ingredientReader= database.getIngredients();
+        return ingredientReader;
     }
-    private int getIngredientCount(int ID){
-        return list.get(ID).getIngredient().size();
+    private int getIngredientCount(){
+        //return list.get(ID).getIngredient().size();
+        ingredientReader= database.getIngredients();
+        return ingredientReader.size();
     }
     public double[] getIngredientsAmount(int id){
-        int count = getIngredientCount(id);
+        ingredientReader= database.getIngredients();
+        int count = getIngredientCount();
         double[] amount = new double[count];
         for(int i = 0; i<count;i++){
-            amount[i] = list.get(id).getIngredient().get(i).getAmount();
+            amount[i] = ingredientReader.get(id).getAmount();
         }
         return amount;
     }
     public String[] getIngredientsUnit(int id){
-        int count = getIngredientCount(id);
+        ingredientReader= database.getIngredients();
+        int count = getIngredientCount();
         String[] unit = new String[count];
         for(int i = 0; i<count;i++){
-            unit[i] = list.get(id).getIngredient().get(i).getUnit();
+            unit[i] = ingredientReader.get(id).getUnit();
         }
         return unit;
     }
     public String[] getIngredientsName(int id){
-        int count = getIngredientCount(id);
+        ingredientReader= database.getIngredients();
+        int count = getIngredientCount();
         String[] name = new String[count];
         for(int i = 0; i<count;i++){
-            name[i] = list.get(id).getIngredient().get(i).getIngredient();
+            name[i] = ingredientReader.get(id).getNameIngredient();
         }
         return name;
     }
